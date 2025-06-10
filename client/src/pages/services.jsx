368cbf7d1af9b668788ccaf29ef42cf963dc75b8
Brill John Torino <btorino@my.centennialcollege.ca>
2025-06-10 19:03:04 -0400

    Fix import case for Navbar and Footer in services.jsx

--- client/src/pages/services.jsx
+++ client/src/pages/services.jsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function Services() {
