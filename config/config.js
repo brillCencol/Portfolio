389e33f1e84c1099cb0102376da6491953c444c5
Brill John Torino <btorino@my.centennialcollege.ca>
2025-06-10 19:25:09 -0400

    Prep backend for Render deployment

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI ||
-        "mongodb+srv://janotorino:KMj8VFX6Lbfkhk5c@cluster0.vmhqfzj.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0" ||
-        process.env.MONGO_HOST ||
-        'mongodb://' + (process.env.IP || 'localhost') + ':' +
-        (process.env.MONGO_PORT || '27017') +
-        '/mernproject'
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
+  mongoUri: process.env.MONGODB_URI ||
+    `mongodb://${process.env.IP || 'localhost'}:${process.env.MONGO_PORT || '27017'}/mernproject`
 }
+
 export default config
